6d78a3dc98a004a927ebabaca300ba0550d2e0ae
taroserigano <taroserigano@gmail.com>
2020-08-23 20:53:51 +0200

    Add files via upload

---
+++ Pics-Finder/src/api/unsplash.js
@@ -0,0 +1,36 @@
+
+
+import axios from 'axios';
+
+
+export default axios.create({
+   baseURL: 'https://api.unsplash.com',
+   headers: {
+       Authorization:
+       'Client-ID tACfbcRY05IZ0r-f0wOFwlR9eSU2gdYBU0iPChv1xUE'
+   }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
